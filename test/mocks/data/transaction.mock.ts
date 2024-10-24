5a9f4199a992362e5af8ae4f748a62ba5d54fdce
Nicole Ordoqui Morales <nicole.ordoqui@gmail.com>
2024-10-24 01:11:47 -0300

    refactor: removed test files

--- test/mocks/data/transaction.mock.ts
+++
@@ -1,8 +0,0 @@
-export const transactionMock = {
-  transaction: {
-    name: 'Instant payment',
-    description: 'Description of my instant payment',
-    amount: 100,
-    currency: 'ARS',
-  },
-};
