3a8965dbe7e001bbddfef2c333c9883be3db89f0
Nicole Ordoqui Morales <nicole.ordoqui@gmail.com>
2024-11-14 10:43:04 -0300

    refactor: test api gateway

--- terraform/modules/lambda/api_gateway.tf
+++ terraform/modules/lambda/api_gateway.tf
@@ -2,22 +2,29 @@ resource "aws_api_gateway_rest_api" "transactions-processing_api-gwt" {
   name = "${var.transactions-processing}_api-gwt"
 }
 
+resource "aws_api_gateway_resource" "transactions-post" {
+  rest_api_id = aws_api_gateway_rest_api.transactions-processing_api-gwt.id
+  parent_id   = aws_api_gateway_rest_api.transactions-processing_api-gwt.root_resource_id
+  path_part   = "transactions"
+}
+
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.transactions-processing_api-gwt.id
   parent_id   = aws_api_gateway_rest_api.transactions-processing_api-gwt.root_resource_id
   path_part   = "{proxy+}"
 }
-resource "aws_api_gateway_method" "post" {
+
+resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.transactions-processing_api-gwt.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "receive" {
+resource "aws_api_gateway_integration" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.transactions-processing_api-gwt.id
   resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.post.http_method
+  http_method = aws_api_gateway_method.proxy.http_method
 
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
